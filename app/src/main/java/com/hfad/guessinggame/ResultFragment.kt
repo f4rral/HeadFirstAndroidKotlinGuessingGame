af4418d1a10fca4f138d1db571f76ee83c7abaa7
f4rral <f4rral@gmail.com>
2024-10-02 17:17:07 +0300

    ViewModelFactory

--- app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.hfad.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +23,12 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ResultViewModel::class.java)
+
         // Текстовое представление заполняется строкой, переданной из GameFragment.
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        binding.wonLost.text = viewModel.result
 
         // По щелчку на кнопке происходит переход к GameFragment.
         binding.newGameButton.setOnClickListener {
